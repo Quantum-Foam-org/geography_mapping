7d75739b64dc9e4c6acb4865692e5248186215c5
Michael Alaimo <michaelalaimo@gmail.com>
2016-03-11 17:40:02 -0500

    Added support to create csv file using ogr2ogr and create a second csv file to hold the polygon information for each state.  The polygon information still needs to be inserted.

--- util/insertMysql.php
+++ util/insertMysql.php
@@ -71,22 +71,119 @@ if ($opt->file === - 1 || $opt->file === FALSE) {
 define ( 'DB_DSN', 'mysql:host=localhost;dbname=' . $opt->name );
 define ( 'DB_USER_NAME', $opt->user );
 define ( 'DB_USER_PASS', $opt->pass );
-class InsertTigerState {																		
+class InsertTigerState {
 	private $sqlOpt;
-
+	
 	public function __construct(SqlOpt $opt) {
 		$this->sqlOpt = $opt;
 		
-		echo "Using TIGER 2015 File " . $this->sqlOpt->file . "\n";
+		echo "Using State TIGER 2015 SHP File " . $this->sqlOpt->file . "\n";
 		echo "Inserting as user " . $this->sqlOpt->user . "\n";
 		echo "Using database name " . $this->sqlOpt->name . "\n";
 		echo "Using table for state data " . $this->sqlOpt->table_state . "\n";
 		echo "Using table for state boundaries " . $this->sqlOpt->table_boundaries . "\n";
 	}
+	
+	private function shpOp() {
+		if (is_readable( $this->sqlOpt->file )) {
+			$finfo = new finfo (FILEINFO_MIME);
+			$pathInfo = pathinfo ( $this->sqlOpt->file );
+			if ($finfo->file ( $this->sqlOpt->file ) !== 'application/octet-stream; charset=binary' || 
+					$pathInfo ['extension'] !== 'shp') {
+				echo "File must be a ESRI Shape File\n";
+				exit ( ( int ) '-' . __LINE__ );
+			}
+			unset ( $finfo );
+		} else {
+			echo "Unable to open file\n";
+			exit ( ( int ) '-' . __LINE__ );
+		}
+		
+		$shpFile = realpath ( $this->sqlOpt->file );
+		
+		$tmpCsvFileState = sys_get_temp_dir().'/insertMysql_state.csv';
+		
+		if ($this->createCSV ( $shpFile, $tmpCsvFileState ) < 0) {
+			echo "Unable to create CSV file of TIGER State information\n";
+			exit ( (int)'-'.__LINE__ );
+		} else {
+			echo "Created CSV file of TIGER State information\n";
+		}
+		
+		$tmpCsvFileBound = sys_get_temp_dir().'/insertMysql_bound.csv';
+		try
+		{
+			$tmpCsvFile = new SplFileObject($tmpCsvFileBound, 'w');
+				
+			if ($this->createGeom ( $shpFile, $tmpCsvFile) < 0) {
+				echo "Unable to create Geometry files of TIGER State information\n";
+				exit ((int)'-'.__LINE__);
+			} else {
+				echo "Created Geometry files of TIGER State information\n";
+			}
+			
+		} catch (RuntimeException $e)
+		{
+			exit((int) '-'.\common\logging\Logger::obj ()->writeException ( $e ) );
+		} finally {
+			unset($this->tmpCsvFile);
+		}
+		
+		return [$tmpCsvFileState, $tmpCsvFileBound];
+	}
+	
+	private function createCSV($shpFile, $tmpCsvFile) {
+		$cmd = 'ogr2ogr -f CSV ' . escapeshellarg($tmpCsvFile) . ' ' .  escapeshellarg($shpFile);
+		\common\logging\Logger::obj()->write('Running command: '.$cmd);
+		passthru ( $cmd, $ret );
+		
+		return $ret;
+	}
+	
+	private function createGeom($shpFile, SplFileObject $tmpCsvFile) {
+		// get feature count
+		ob_start();
+		$cmd = 'ogrinfo  -so '. escapeshellarg($shpFile).'  tl_2015_us_state';
+		\common\logging\Logger::obj()->write('Running command: '.$cmd);
+		passthru( $cmd, $ret);
+		$output = ob_get_contents();
+		ob_end_clean();
+		
+		if (preg_match('/Feature\sCount:\s(\d+)/', $output, $matches) === 1 && 
+				($fids = filter_var($matches[1], FILTER_VALIDATE_INT))) {
+			
+			\common\logging\Logger::obj()->write('Features: '.$fids);
+			
+			for ($fid = 0; $fid < $fids; $fid++) {
+				ob_start();
+				$cmd = 'ogrinfo -fid '. $fid.' '. escapeshellarg($shpFile).' tl_2015_us_state | grep POLYGON';
+
+				\common\logging\Logger::obj()->write('Running command: '.$cmd);
+				passthru($cmd, $ret );
+				if ($ret >= 0) {
+					\common\logging\Logger::obj()->write('Writing Feature: '.$fid);
+					$output = trim(ob_get_contents());
+					$tmpCsvFile->fputcsv([$fid, $output]);
+				} else {
+					echo "Unable to write feature $fid to output file\n";
+				}
+				ob_end_clean();
+			}
+		} else {
+			echo "Unable to determine the feature count of the shape file\n";
+			exit((int)'-'.__LINE__);
+		}
+		
+		return $ret;
+	}
+	
 	public function run() {
 		$db = db::obj ();
+		
+		list($tmpCsvFileState, $tmpCsvFileBound) = $this->shpOp();
+		
 		try {
-			$f = new SplFileObject ( $this->sqlOpt->file, 'r' );
+			$f = new SplFileObject ( $tmpCsvFileState, 'r' );
 			$f->setFlags ( SplFileObject::SKIP_EMPTY | SplFileObject::DROP_NEW_LINE );
 			$headers = array_slice ( array_map ( function ($v) {
 				return strtolower ( $v );
@@ -98,18 +195,19 @@ class InsertTigerState {
 				$point = "ST_PointFromText('POINT({$row[12]} {$row[13]})')";
 				try {
 					$sth = $db->getSth ( 'INSERT INTO `' . $this->sqlOpt->table_state . '` (`' . implode ( '`, `', $headers ) . '`) VALUES(?' . str_repeat ( ', ?', count ( $values ) - 1 ) . ', ' . $point . ')', $values );
+				} catch ( \RuntimeException $e ) {
+					\common\logging\Logger::obj ()->writeException ( $e );
 				}
-	            catch ( \RuntimeException $e ) {
-	            	\common\logging\Logger::obj()->writeException($e);
-	            }
 			}
 			unset ( $row, $values );
 		} catch ( RuntimeException $e ) {
-			exit ( \common\logging\Logger::obj()->writeException($e));
+			exit ((int)'-'. \common\logging\Logger::obj ()->writeException ( $e ) );
 		} finally {
 			unset ( $f, $md5, $fileName );
 		}
 		unset ( $db );
+		unlink($tmpCsvFileState);
+		unlink($tmpCsvFileBound);
 	}
 }
 
